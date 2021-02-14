bd7bb4189daf387377fb48009a91fbc02214a8e1
reginadiana <dianaregina22@outlook.com.br>
2021-02-14 18:52:58 -0300

    Refactory footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,34 +3,32 @@ import { Link } from "react-router-dom";
 import * as Styled from "./style.js";
 import { linkedinProfile } from "../../services/linkedin";
 
-const Footer = () => {
-  return (
-    <Styled.Container>
-      <Styled.Lists>
-        <ol>
-          <h3>Nossas Desenvolvedoras</h3>
-          {linkedinProfile.map(({ name, link }) => (
-            <li>
-              <a href={link}>{name}</a>
-            </li>
-          ))}
-        </ol>
-        <ol>
-          <h3>Gerador de Certificados</h3>
+const Footer = () => (
+  <Styled.Container>
+    <Styled.Lists>
+      <ol>
+        <h3>Nossas Desenvolvedoras</h3>
+        {linkedinProfile.map(({ name, link }) => (
           <li>
-            <Link to="/sign-in">Login</Link>
+            <a href={link}>{name}</a>
           </li>
-          <li>
-            <Link to="/sign-up">Cadastro</Link>
-          </li>
-        </ol>
-      </Styled.Lists>
+        ))}
+      </ol>
+      <ol>
+        <h3>Gerador de Certificados</h3>
+        <li>
+          <Link to="/sign-in">Login</Link>
+        </li>
+        <li>
+          <Link to="/sign-up">Cadastro</Link>
+        </li>
+      </ol>
+    </Styled.Lists>
 
-      <Styled.Licence>
-        <h4>Licença MIT - Copyright - 2020 - Certificates for Everyone</h4>
-      </Styled.Licence>
-    </Styled.Container>
-  );
-};
+    <Styled.Licence>
+      <h4>Licença MIT - Copyright - 2020 - Certificates for Everyone</h4>
+    </Styled.Licence>
+  </Styled.Container>
+);
 
 export default Footer;
